821e75ab8b118b209e5837e0af9feb8158e47ff5
Charles Kerr <charles.kerr@canonical.com>
2016-07-01 10:17:06 -0500

    extract the xdg user dirs sandbox generator into a standalone class for reuse by multiple tests

---
+++ tests/utils/xdg-user-dirs-sandbox.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Charles Kerr <charles.kerr@canonical.com>
+ */
+
+#pragma once
+
+#include <memory> // std::shared_ptr
+
+class QTemporaryDir;
+
+class XdgUserDirsSandbox
+{
+public:
+    XdgUserDirsSandbox();
+
+private:
+    void init();
+    std::shared_ptr<QTemporaryDir> temporary_dir_;
+};
+
