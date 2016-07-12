a766251e08b025e3e3400ef6bcdbedac6ba99fcc
Charles Kerr <charles.kerr@canonical.com>
2016-07-12 15:17:22 -0500

    add 'Helper' base class that has a state Q_PROPERTY for (not-started, started, completed, failed, cancelled)

---
+++ include/helper/helper.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2016 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Charles Kerr <charles.kerr@canonical.com>
+ */
+
+#pragma once
+
+#include <QObject>
+
+class Helper: public QObject
+{
+    Q_OBJECT
+
+public:
+    virtual ~Helper();
+    Q_DISABLE_COPY(Helper)
+
+    enum class State {NOT_STARTED, STARTED, CANCELLED, FAILED, COMPLETE};
+    State getState() const;
+    Q_PROPERTY(State state READ getState NOTIFY stateChanged)
+
+Q_SIGNALS:
+    void stateChanged(const State& new_state);
+
+protected:
+    Helper(QObject *parent=nullptr);
+    void setState(State);
+
+private:
+    State state_;
+};
---
+++ src/helper/helper.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2016 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Charles Kerr <charles.kerr@canonical.com>
+ */
+
+#include <helper/helper.h>
+
+#include <QDebug>
+
+namespace
+{
+
+QString toString(Helper::State state)
+{
+    QString ret = QStringLiteral("bug");
+
+    switch (state)
+    {
+        case Helper::State::NOT_STARTED: ret = QStringLiteral("not-started"); break;
+        case Helper::State::STARTED:     ret = QStringLiteral("started");     break;
+        case Helper::State::CANCELLED:   ret = QStringLiteral("cancelled");   break;
+        case Helper::State::FAILED:      ret = QStringLiteral("failed");      break;
+        case Helper::State::COMPLETE:    ret = QStringLiteral("complete");    break;
+    }
+
+    return ret;
+}
+
+} // anon namespace
+
+/***
+****
+***/
+
+Helper::Helper(QObject *parent)
+    : QObject(parent)
+    , state_{State::NOT_STARTED}
+{
+}
+
+Helper::~Helper() =default;
+
+Helper::State
+Helper::getState() const
+{
+    return state_;
+}
+
+void
+Helper::setState(State state)
+{
+    if (state_ != state)
+    {
+
+        qDebug() << "changing state of helper" << static_cast<void*>(this) << "from" << toString(state_) << "to" << toString(state);
+
+        state_ = state;
+
+        Q_EMIT stateChanged(state);
+    }
+}
