bc55a6100343661fd81c2f203b6e91e5506dcc45
Charles Kerr <charles.kerr@canonical.com>
2016-08-19 11:08:39 -0500

    (copyediting) in storage-framework-client, remove unused #includes

--- src/storage-framework/storage_framework_client.cpp
+++ src/storage-framework/storage_framework_client.cpp
@@ -15,20 +15,12 @@
  *
  * Author: Xavi Garcia <xavi.garcia.mena@canonical.com>
  */
-#include <QLocalSocket>
+
 #include "storage_framework_client.h"
 
-
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <time.h>
+#include <QDateTime>
+#include <QLocalSocket>
+#include <QString>
 
 using namespace unity::storage::qt::client;
 
