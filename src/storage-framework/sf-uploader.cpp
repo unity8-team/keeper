fa23c465452138a7ff7e70a954a5af93907e46e9
Charles Kerr <charles.kerr@canonical.com>
2016-09-05 13:27:08 -0500

    add Uploader wrapper

---
+++ src/storage-framework/sf-uploader.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Charles Kerr <charles.kerr@canonical.com>
+ */
+
+#include "storage-framework/sf-uploader.h"
+
+#include <QFuture>
+#include <QFutureWatcher>
+
+StorageFrameworkUploader::StorageFrameworkUploader(
+    std::shared_ptr<unity::storage::qt::client::Uploader> const& uploader,
+    QObject *parent
+):
+    Uploader(parent),
+    uploader_(uploader)
+{
+}
+
+std::shared_ptr<QLocalSocket>
+StorageFrameworkUploader::socket()
+{
+    return uploader_->socket();
+}
+
+void
+StorageFrameworkUploader::commit()
+{
+    qDebug() << "StorageFrameworkClient::finish() is committing";
+
+    std::function<void(std::shared_ptr<unity::storage::qt::client::File> const&)> on_finished =
+        [this](std::shared_ptr<unity::storage::qt::client::File> const& /*file*/){
+            Q_EMIT(committed());
+        };
+
+    connections_.connect_future(uploader_->finish_upload(), on_finished);
+}
