e41dec8a4d47d1967de4a0b586cce87ce5f1033f
Charles Kerr <charles.kerr@canonical.com>
2016-09-13 20:21:26 -0500

    in StorageFrameworkUploader::commit(), make the 'success' flag emitted conditional on whether or not the QFuture yields a valid File

--- src/storage-framework/sf-uploader.cpp
+++ src/storage-framework/sf-uploader.cpp
@@ -45,8 +45,8 @@ StorageFrameworkUploader::commit()
     connections_.connect_future(
         uploader_->finish_upload(),
         std::function<void(std::shared_ptr<unity::storage::qt::client::File> const&)>{
-            [this](std::shared_ptr<unity::storage::qt::client::File> const& /*file*/){
-                bool success = true; // FIXME
+            [this](std::shared_ptr<unity::storage::qt::client::File> const& file){
+                auto const success = bool(file);
                 qDebug() << "commit finished with" << success;
                 Q_EMIT(commit_finished(success));
             }
