4c78277038a4a2c5804d8cd2ebdaa3a8f437810a
Xavi Garcia Mena <xavi.garcia.mena@canonical.com>
2016-09-15 17:49:47 +0200

    Decreased the number of files we fill in temporary directories for integration test

--- tests/integration/helpers/helpers-test.cc
+++ tests/integration/helpers/helpers-test.cc
@@ -80,7 +80,7 @@ TEST_F(TestHelpers, StartFullTest)
     qDebug() << "USER DIR:" << user_dir;
 
     // fill something in the music dir
-    FileUtils::fillTemporaryDirectory(user_dir, qrand() % 1000);
+    FileUtils::fillTemporaryDirectory(user_dir, qrand() % 100);
 
     // search for the user folder uuid
     auto user_folder_uuid = get_uuid_for_xdg_folder_path(user_dir, choices.value());
@@ -94,7 +94,7 @@ TEST_F(TestHelpers, StartFullTest)
     qDebug() << "USER DIR 2:" << user_dir_2;
 
     // fill something in the music dir
-    FileUtils::fillTemporaryDirectory(user_dir_2, qrand() % 1000);
+    FileUtils::fillTemporaryDirectory(user_dir_2, qrand() % 100);
 
     // search for the user folder uuid
     auto user_folder_uuid_2 = get_uuid_for_xdg_folder_path(user_dir_2, choices.value());
