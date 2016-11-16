f3fac033b222e48aa1cd951aff807e9e21102ade
Xavi Garcia Mena <xavi.garcia.mena@canonical.com>
2016-11-16 15:24:25 +0100

    Fix copyright issue

--- src/test-restore/test-restore-socket.cpp
+++ src/test-restore/test-restore-socket.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2016 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Charles Kerr <charles.kerr@canonical.com>
+ *   Xavi Garcia Mena <xavi.garcia.mena@canonical.com>
+ */
+
 #include "test-restore-socket.h"
 
 #include "storage-framework/storage_framework_client.h"
