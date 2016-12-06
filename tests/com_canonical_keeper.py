d653c767536f1c1a50ef2ce6d6c6aaf233f5cbf7
Charles Kerr <charles.kerr@canonical.com>
2016-12-06 07:41:35 -0600

    make copyright test happy

--- tests/com_canonical_keeper.py
+++ tests/com_canonical_keeper.py
@@ -12,6 +12,8 @@ from gi.repository import GLib
 '''com.canonical.keeper mock template
 '''
 
+# (c) 2016 Canonical Ltd.
+#
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation; either version 3 of the License, or (at your option) any
